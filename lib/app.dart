6140f16491cdeba913bf78db107f42a1611bcda0
Ahmed Ekramy <132719533+Ahmed-Ekramy@users.noreply.github.com>
2023-11-15 02:08:49 -0900

    add structure App

---
+++ lib/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CheckoutApp extends StatelessWidget {
+  const CheckoutApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    );
+  }
+}
