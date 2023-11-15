73cc6cee5932da5127a9775566043af79fb1ba24
Ahmed Ekramy <132719533+Ahmed-Ekramy@users.noreply.github.com>
2023-11-15 08:38:03 -0900

    add textStyle App

---
+++ lib/core/utils/text_style.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+TextStyle inter25W500()=>GoogleFonts.inter(
+  fontSize: 20.sp, fontWeight: FontWeight.w500,
+);
+TextStyle inter18W400()=>GoogleFonts.inter(
+  fontSize: 18.sp, fontWeight: FontWeight.w400,
+);
+TextStyle inter18W600()=>GoogleFonts.inter(
+  fontSize: 18.sp, fontWeight: FontWeight.w600,
+);
+TextStyle inter20W600()=>GoogleFonts.inter(
+  fontSize: 20.sp, fontWeight: FontWeight.w600,
+);
+TextStyle inter20W400()=>GoogleFonts.inter(
+  fontSize: 20.sp, fontWeight: FontWeight.w400,
+  color: Colors.black.withOpacity(0.800000011920929),
+);
+TextStyle inter24W600()=>GoogleFonts.inter(
+  fontSize: 24.sp, fontWeight: FontWeight.w600,
+);
+TextStyle inter22W500()=>GoogleFonts.inter(
+  fontSize: 22.sp, fontWeight: FontWeight.w500,
+);
